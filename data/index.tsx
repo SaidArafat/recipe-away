2718aa86ca9da423f2b5f9b80b5f361045b631bb
Said Arafat <s.arafat.selmi@gmail.com>
2023-08-09 16:41:05 +0300

    Add data

---
+++ data/index.tsx
@@ -0,0 +1,80 @@
+export const popular = [
+  {
+    id: 1,
+    title: "Fried chicken with rice rames",
+    image:
+      "https://images.pexels.com/photos/8511837/pexels-photo-8511837.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 2,
+    title: "Fried chicken with rice rames",
+    image:
+      "https://images.pexels.com/photos/8511837/pexels-photo-8511837.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 3,
+    title: "Fried chicken with rice rames",
+    image:
+      "https://images.pexels.com/photos/8511837/pexels-photo-8511837.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 4,
+    title: "Fried chicken with rice rames",
+    image:
+      "https://images.pexels.com/photos/8511837/pexels-photo-8511837.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 5,
+    title: "Fried chicken with rice rames",
+    image:
+      "https://images.pexels.com/photos/8511837/pexels-photo-8511837.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+];
+
+export const feedbacks = [
+  {
+    id: 1,
+    message:
+      "The quality of food recipes is very good because it is given a tutorial by a reliable chef, and everyone can follow it thank you",
+    title: "Alex K",
+    job: "AI developer",
+    image:
+      "https://images.pexels.com/photos/1559486/pexels-photo-1559486.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 4,
+    message:
+      "The food recipes here are top-quality, with step-by-step guidance from a trustworthy chef. They are suitable for cooks of all levels, ensuring success in the kitchen.",
+    title: "Lucas Turner",
+    job: "Architect",
+    image:
+      "https://images.pexels.com/photos/5384445/pexels-photo-5384445.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 5,
+    message:
+      "The provided food recipes with tutorials from a reliable chef are of exceptional quality. It's a wonderful opportunity for everyone to explore their culinary skills. Thank you!",
+    title: "Samuel Brooks",
+    job: "Back-end",
+    image:
+      "https://images.pexels.com/photos/5490276/pexels-photo-5490276.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+];
+
+export const options = [
+  "carrot",
+  "broccoli",
+  "asparagus",
+  "cauliflower",
+  "corn",
+  "cucumber",
+  "green pepper",
+  "lettuce",
+  "mushrooms",
+  "onion",
+  "potato",
+  "pumpkin",
+  "red pepper",
+  "tomato",
+  "beetroot",
+];
